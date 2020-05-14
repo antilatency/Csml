9b3bd728e5d5ceab95f9bc0ad8b2d4d12dbe48b2
Nikita Baskakov <ooogpo@gmail.com>
2020-05-14 20:07:52 +0300

    [Downloadable] icons, file filter

--- Src/Downloadable/Downloadable.cs
+++ Src/Downloadable/Downloadable.cs
@@ -6,35 +6,68 @@ using System.Security.Cryptography;
 using System.IO.Compression;
 
 using HtmlAgilityPack;
+using System.Linq;
 
 namespace Csml
 {
-    public class DownloadableCache: Cache<DownloadableCache> {
+    public class DownloadableCache : Cache<DownloadableCache> { }
+
+    public enum  DownloadableIcon {
+        None,
+
+        Windows,
+        Android, 
+        Linux,
+
+        Zip,
+        Cloud
     }
 
     public sealed class Downloadable : Element<Downloadable> {
 
+        public static readonly CompressionLevel ZipCompressionLevel = CompressionLevel.Optimal;
+
         private string SourcePath { get; set; }
         private string DownloadableFileName { get; set; }
+        private DownloadableIcon Icon { get; set; }
         private string Tooltip { get; set; }
+        private bool Zip { get; set; }
+        private List<string> ZipEntries { get; set; }
 
         private DownloadableCache _cache = null;
 
-        public Downloadable(string sourcePath, string tooltip = "") {
+        public Downloadable(string sourcePath, DownloadableIcon icon = DownloadableIcon.None, string tooltip = "", Func<string, bool> packToArchivePredicate = null) {
             SourcePath = ConvertPathToAbsolute(sourcePath);
 
             if (!IsValidSource(SourcePath)) {
                 Log.Error.Here($"Invalid downloadable source = {SourcePath}");
             }
 
-            DownloadableFileName = GetDownloadableFileNameFromSource(SourcePath);
+            Zip = IsNeedToBeArchived(SourcePath);
+            DownloadableFileName = GetDownloadableFileNameFromSource(SourcePath, Zip);
+            Icon = icon;
             Tooltip = tooltip;
+            ZipEntries = null;
+
+            if (Zip) {
+                if (IsDirectory(SourcePath)) {
+                    var entries = Directory.GetFiles(SourcePath, "*", SearchOption.AllDirectories);
+
+                    if (packToArchivePredicate != null) {
+                        ZipEntries = new List<string>(entries.Where(packToArchivePredicate));
+                    } else {
+                        ZipEntries = new List<string>(entries);
+                    }
+                } else {
+                    ZipEntries = new List<string>(new string[] { SourcePath });
+                }
+            } 
         }
 
-        private string GetDownloadableFileNameFromSource(string sourcePath) {
+        private string GetDownloadableFileNameFromSource(string sourcePath, bool zip) {
             var name = IsDirectory(sourcePath) ? new DirectoryInfo(sourcePath).Name : Path.GetFileName(sourcePath);
 
-            return IsNeedToBeArchived(sourcePath) ? name + ".zip" : name;
+            return zip ? name + ".zip" : name;
         }
 
         private bool IsValidSource(string sourcePath) {
@@ -50,31 +83,10 @@ namespace Csml
         }
 
         private string CalculateHash(string sourcePath) {
-            var md5 = new MD5CryptoServiceProvider();
-            Func<string, byte[]> getFileHash = (filePath) => {
-                using (var fileStream = File.OpenRead(filePath)) {
-                    using (var bufferedStream = new BufferedStream(fileStream, 1000000)) {
-                        return md5.ComputeHash(bufferedStream);
-                    }
-                }
-            };
-
-            Func<byte[], string> binToString = (data) => {
-                var hashString = new StringBuilder();
-                for (int i = 0; i < data.Length; i++) {
-                    hashString.Append(data[i].ToString("x2"));
-                }
-                return hashString.ToString();
-            };
-
-            if (IsDirectory(sourcePath)) {
-                var files = Directory.GetFiles(sourcePath, "*", SearchOption.AllDirectories);
-                var hashes = new List<byte>(files.Length * 32);
-                files.ForEach(x => hashes.AddRange(getFileHash(x)));
-
-                return binToString(md5.ComputeHash(hashes.ToArray()));
+            if (Zip && ZipEntries != null && ZipEntries.Count > 0) {
+                return HashUtils.HashToString(HashUtils.GetFilesHash(ZipEntries));
             } else {
-                return binToString(getFileHash(sourcePath));
+                return HashUtils.HashToString(HashUtils.GetFileHash(sourcePath));
             }
         }
 
@@ -89,8 +101,12 @@ namespace Csml
 
                     var outputPath = Path.Combine(_cache.Directory, DownloadableFileName);
 
-                    if (IsNeedToBeArchived(SourcePath)) {
-                        ZipFile.CreateFromDirectory(SourcePath, outputPath);
+                    if (Zip) {
+                        using (var zip = ZipFile.Open(outputPath, ZipArchiveMode.Create)) {
+                            foreach (var path in ZipEntries) {
+                                zip.CreateEntryFromFile(path, Path.GetRelativePath(SourcePath, path), ZipCompressionLevel);
+                            }
+                        }
                     } else {
                         File.Copy(SourcePath, outputPath);
                     }
@@ -102,16 +118,34 @@ namespace Csml
             return _cache.GetFileUri(DownloadableFileName);
         }
 
+        private string GetIconCssClass(DownloadableIcon icon) {
+            if (icon != DownloadableIcon.None) {
+                return string.Format("DownloadableIcon{0}", icon.ToString());
+            }
+
+            return string.Empty;
+        }
+
         public override IEnumerable<HtmlNode> Generate(Context context) {
             var content = GetContent();
             var button = HtmlNode.CreateNode("<a>");
 
             button.AddClass("Downloadable");
             button.SetAttributeValue("href", content.ToString());
+            button.SetAttributeValue("download", DownloadableFileName);
             button.InnerHtml = DownloadableFileName;
 
             if (!string.IsNullOrEmpty(Tooltip)) {
                 button.SetAttributeValue("title", Tooltip);
+            }
+
+            if (Icon != DownloadableIcon.None) {
+                var icon = HtmlNode.CreateNode("<i>");
+
+                icon.AddClass("DownloadableIcon");
+                icon.AddClass(GetIconCssClass(Icon));
+
+                button.AppendChild(icon);
             }
 
             yield return button;
