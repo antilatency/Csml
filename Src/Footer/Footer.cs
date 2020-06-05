4db53e065dbe18d5198eae048489c0560725ebd3
Nikita Baskakov <ooogpo@gmail.com>
2020-06-05 11:44:10 +0300

    footer

---
+++ Src/Footer/Footer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace Csml {
+    public sealed class Footer : Collection<Footer>  {
+        public Footer()  { }
+
+        public override IEnumerable<HtmlNode> Generate(Context context) {
+            var footer = HtmlNode.CreateNode("<footer>");
+            var container = footer.AppendChild(HtmlNode.CreateNode("<div>"));
+
+            footer.AddClass("Footer");
+            container.AddClass("FooterContainer");
+
+            container.Add(base.Generate(context));
+
+            yield return footer;
+        }
+    }
+}
---
+++ Src/Footer/FooterMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace Csml {
+    public sealed class FooterMenu : Container<FooterMenu> {
+        public FooterMenu() : base("nav", "FooterMenu") { }
+    }
+
+    public sealed class FooterMenuSection : Collection<FooterMenuSection> {
+        public string Title { get; private set; }
+        public FooterMenuSection(string title) {
+            Title = title;
+        }
+
+        public override IEnumerable<HtmlNode> Generate(Context context) {
+            var section = HtmlNode.CreateNode("<div>");
+
+            section.AddClass("FooterMenuSection");
+
+            if (!string.IsNullOrEmpty(Title)) {
+                section.AppendChild(HtmlNode.CreateNode($"<h5 style=\"margin-bottom: 12px;\">{Title}</h5>"));
+            }
+
+            section.Add(base.Generate(context));
+
+            yield return section;
+        }
+    }
+
+    public sealed class FooterMenuSectionList : ContainerWithSpecificChildren<FooterMenuSectionList> {
+        public FooterMenuSectionList() : base("ul", "li", "FooterMenuSectionList") {
+
+        }
+    }
+}
