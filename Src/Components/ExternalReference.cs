29a0d8b05acd980b7898bafec5b1befc2d0dd978
Nikita Baskakov <ooogpo@gmail.com>
2020-06-16 19:07:36 +0300

    external references with image

--- Src/Components/ExternalReference.cs
+++ Src/Components/ExternalReference.cs
@@ -6,20 +6,31 @@ namespace Csml {
     public class ExternalReference : Element<ExternalReference> {
         private Uri Href { get; set; }
         private string Text { get; set; }
+        private Image Image { get; set; }
         private string Tooltip { get; set; }
 
 
         public ExternalReference(string href, string text = "", string tooltip = "") {
-            try {
-                Href = new Uri(href);
-            }
-            catch {
-                Log.Error.OnCaller("Invalid href " + href);
-            }
+            SetHref(href);
             Text = text;
+            Image = null;
             Tooltip = tooltip;
         }
 
+        public ExternalReference(string href, Image image, string tooltip = "") {
+            SetHref(href);
+            Text = string.Empty;
+            Image = image;
+            Tooltip = tooltip;
+        }
+
+        private void SetHref(string href) {
+            try {
+                Href = new Uri(href);
+            } catch {
+                Log.Error.OnCaller("Invalid href " + href);
+            }
+        }
 
         public override IEnumerable<HtmlNode> Generate(Context context) {
 
@@ -28,10 +39,14 @@ namespace Csml {
                 if (!context.AForbidden)
                     x.SetAttributeValue("href", Href.ToString());
 
-                if (string.IsNullOrEmpty(Text))
+                if (Image != null) {
+                    x.Add(Image.Generate(context));
+                } else if (string.IsNullOrEmpty(Text)) {
                     x.InnerHtml = Href.ToString();
-                else
+                } else {
                     x.InnerHtml = Text;
+                }
+                    
                 if (!string.IsNullOrEmpty(Tooltip)) {
                     x.SetAttributeValue("title", Tooltip);
                 } else {
