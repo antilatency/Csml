85b2d631cd091ff9c9ad32fe38310547c4411485
Nikita Baskakov <ooogpo@gmail.com>
2020-09-07 18:29:13 +0300

    [Fix] add IEnumerable<IElement> to lists

--- Src/Components/ContainerWithSpecificChildren.cs
+++ Src/Components/ContainerWithSpecificChildren.cs
@@ -16,12 +16,32 @@ namespace Csml {
 
         public override Node Generate(Context context) {
             var result = new Tag(Tag).AddClasses(Classes);
-            foreach (var i in Elements) {
+
+
+            /*foreach (var i in Elements) {
                 result.AddTag(ChildTag, x => {
                     x.Add(i.Generate(context));
                 });
-            }
+            }*/
+
+
+            Generate(result, Elements, context);
+
+
             return result;
+        }
+
+        private void Generate(Tag parent, IEnumerable<IElement> elements, Context context) {
+            foreach (var i in elements) {
+                if (i is LazyCollection collection) {
+                    Generate(parent, collection.Elements, context);
+                } else {
+                    parent.AddTag(ChildTag, x => {
+                        x.Add(i.Generate(context));
+                    });
+                }
+                
+            }
         }
     }
 }
