6c3c28f891d24b1b6668fec3d77e47348387f972
nikita-semenikhin <nikita-semenikhin@antilatency.com>
2020-12-03 13:17:46 +0300

    minor fixes

---
+++ Src/Page/Material.js
@@ -0,0 +1,8 @@
+function Material(element) {
+    this.OnResourcesLoaded = function() {
+        let path = window.location.pathname;
+        if (element.href.split('/').reverse()[0] == path.split('/').reverse()[0]) {
+            element.style.backgroundColor = "#333333";
+        }
+    }
+}
